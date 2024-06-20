befd0d30a5d26fec9a01c360655c2ba492d9e112
Declan Naughton <declann1990@gmail.com>
2024-06-20 14:43:06 +0100

    s

--- random.js
+++ random.js
@@ -3,4 +3,4 @@
 import { prng_alea } from 'https://cdn.jsdelivr.net/npm/esm-seedrandom/+esm'
 
 // prng_alea call being memoised is bad (todo fix at lower-level)
-export const seeded = d => prng_alea(d);
+export const seeded = prng_alea("seed");
