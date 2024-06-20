d6539d22c9ded3037be6a18fd1aaaca4584e9d6d
Declan Naughton <declann1990@gmail.com>
2024-06-20 14:41:09 +0100

    random hack

---
+++ random.js
@@ -0,0 +1,6 @@
+
+// seeded random
+import { prng_alea } from 'https://cdn.jsdelivr.net/npm/esm-seedrandom/+esm'
+
+// prng_alea call being memoised is bad (todo fix at lower-level)
+export const seeded = d => prng_alea(d);
