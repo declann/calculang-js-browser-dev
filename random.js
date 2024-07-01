f4586045150a19a70d9358fd695f187c065f8de5
Declan Naughton <declann1990@gmail.com>
2024-07-01 11:42:31 +0100

    prng seed comments

--- random.js
+++ random.js
@@ -4,3 +4,8 @@ import { prng_alea } from 'https://cdn.jsdelivr.net/npm/esm-seedrandom/+esm'
 
 // prng_alea call being memoised is bad (todo fix at lower-level)
 export const seeded = prng_alea("seed");
+
+// todo use s/t like this instead:
+// https://news.ycombinator.com/item?id=40840616 (+ see other comments)
+// but I do need to look at interop with JS here
+
