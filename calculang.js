55c8ed1062382589f86f8581693a5a2caae6a2dd
Declan Naughton <declann1990@gmail.com>
2024-07-12 08:47:02 +0100

    pin babel version

--- calculang.js
+++ calculang.js
@@ -14,7 +14,7 @@
 //import {require} from "https://cdn.jsdelivr.net/npm/d3-require/+esm";
 
 //import * as Babel from 'https://unpkg.com/@babel/standalone/babel.js';
-import * as Babel from 'https://cdn.jsdelivr.net/npm/@babel/standalone/+esm'
+import * as Babel from 'https://cdn.jsdelivr.net/npm/@babel/standalone@7.24.7/+esm'
 //const Babel = await require("@babel/standalone")
 
 
